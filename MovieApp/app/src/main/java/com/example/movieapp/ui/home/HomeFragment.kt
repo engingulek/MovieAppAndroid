c61acdc76f15c54f03a62d1c9e33073aa45a19cc
Engin Gülek <74055938+engingulek@users.noreply.github.com>
2024-11-14 18:28:40 +0300

    finished homefragment design

--- MovieApp/app/src/main/java/com/example/movieapp/ui/home/HomeFragment.kt
+++ MovieApp/app/src/main/java/com/example/movieapp/ui/home/HomeFragment.kt
@@ -6,9 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.movieapp.R
 import com.example.movieapp.databinding.FragmentHomeBinding
+import com.example.movieapp.ui.home.adapters.CategoryAdapter
+import com.example.movieapp.ui.home.adapters.ForYouMovieAdapter
+import com.example.movieapp.ui.home.adapters.TrendingMovieAdapter
 
 
 class HomeFragment : Fragment() {
@@ -19,10 +23,16 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         design = DataBindingUtil.inflate(inflater,R.layout.fragment_home, container, false)
-        design.trendingMovieRyc.layoutManager = LinearLayoutManager(requireContext(),
-            LinearLayoutManager.HORIZONTAL,false)
+        design.trendingMovieRyc.layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.HORIZONTAL,false)
         val trendingMovieAdapter = TrendingMovieAdapter(requireContext())
-        design.adapter = trendingMovieAdapter
+        design.trendingMovieAdapter = trendingMovieAdapter
+        design.categoryRyc.layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.HORIZONTAL,false)
+        val categoryAdapter = CategoryAdapter(requireContext())
+        design.categoryAdapter = categoryAdapter
+
+        design.forYouRyc.layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.HORIZONTAL,false)
+        val forYouAdapter = ForYouMovieAdapter(requireContext())
+        design.forYouAdapter = forYouAdapter
         return  design.root
     }
 
